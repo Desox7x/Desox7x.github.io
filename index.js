591fc01ce1ca44bd864ae29feffe25260a4e3c58
Desox7x <agenao1997@gmail.com>
2023-01-26 10:36:54 -0400

    Cleaning and Refactoring index.js

--- index.js
+++ index.js
@@ -1,52 +1,33 @@
-
-function val() {
-
-    const stateOption = document.getElementById("state").value;
-
-
-  (async () => {
-    const url =
-      "https://parseapi.back4app.com/classes/Usabystate_" +
-      stateOption +
-      "?&keys=name";
-    const response = await fetch(url, {
-      headers: {
-        "X-Parse-Application-Id": "4rN8qhOFKzQTnO1kOYar42acWUH5hPF9LdBYIGbK", // This is your app's application id
-        "X-Parse-REST-API-Key": "Tcxg3oxHJuEf3BGWOlmiQpSAPRX4bdn3RXqBpNbe", // This is your app's REST API key
-      },
-    });
-
-    let dataPrev = await response.json(); // Here you have the data that you need
-
-    const dataString = JSON.stringify(dataPrev, null, 2);
-    dataPrev = JSON.parse(dataString);
-
-    const data = Object.values(dataPrev);
-    console.log(data);
-
-    try {
-      console.log(data[0][1].name);
-    } catch (error) {}
-    // console.log(data);
-
-    let ele = document.getElementById("city");
-
-    let option;
-
-    while (ele.firstChild) {
-        ele.firstChild.remove()
-    }
-
-    console.log(data);
-    for (let i = 0; i < data[0].length; i++) {
-      option = document.createElement("option");
-      option.value = data[0][i].name;
-      option.text = data[0][i].name;
-       
-      console.log(data[0][i].name);
-      ele.add(option);
-    }
-  })();
+// This function gets the value of the selected state and calls the fetchData function
+function valueChange() {
+  const stateOption = document.getElementById("state").value;
+  fetchData(stateOption);
 }
 
-//
+// This function fetches data from the API URL and calls the handleData function
+async function fetchData(stateOption) {
+  const url = `https://parseapi.back4app.com/classes/Usabystate_${stateOption}?&keys=name`;
+  const headers = {
+    "X-Parse-Application-Id": "4rN8qhOFKzQTnO1kOYar42acWUH5hPF9LdBYIGbK",
+    "X-Parse-REST-API-Key": "Tcxg3oxHJuEf3BGWOlmiQpSAPRX4bdn3RXqBpNbe",
+  };
+  const response = await fetch(url, { headers });
+  const dataPrev = await response.json();
+  const data = Object.values(dataPrev);
+  handleData(data);
+}
+
+// This function handle the data received and update the city field options
+function handleData(data) {
+  const ele = document.getElementById("city");
+  while (ele.firstChild) {
+    ele.firstChild.remove();
+  }
+  for (let i = 0; i < data[0].length; i++) {
+    const option = document.createElement("option");
+    option.value = data[0][i].name;
+    option.text = data[0][i].name;
+    ele.add(option);
+  }
+}
+
